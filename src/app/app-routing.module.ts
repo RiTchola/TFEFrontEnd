2d7d66d675a18b9db24adb90e5c944e7c8f73cbe
Donchi Donald <donchidonald2000@gmail.com>
2023-09-02 23:59:32 +0200

    #8: code update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,14 +10,14 @@ import { AppLayoutComponent } from "./layout/app.layout.component";
                 path: '', component: AppLayoutComponent,
                 children: [
                     { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-                    { path: 'dashboard', loadChildren: () => import('./features/modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
-                    { path: 'calendrier', loadChildren: () => import('./features/modules/calendrier/calendrier.module').then(m => m.CalendrierModule) },
-                    { path: 'gestionnaire', loadChildren: () => import('./features/modules/gestionnaire/gestionnaire.module').then(m => m.GestionnaireModule) },
-                    { path: 'outils', loadChildren: () => import('./features/modules/outils/outils.module').then(m => m.OutilsModule) },
-                    { path: 'parametre', loadChildren: () => import('./features/modules/parametre/parametre.module').then(m => m.ParametreModule) },
+                    { path: 'dashboard', loadChildren: () => import('./features/pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+                    { path: 'calendrier', loadChildren: () => import('./features/pages/calendrier/calendrier.module').then(m => m.CalendrierModule) },
+                    { path: 'gestionnaire', loadChildren: () => import('./features/pages/gestionnaire/gestionnaire.module').then(m => m.GestionnaireModule) },
+                    { path: 'outils', loadChildren: () => import('./features/pages/outils/outils.module').then(m => m.OutilsModule) },
+                    { path: 'parametre', loadChildren: () => import('./features/pages/parametre/parametre.module').then(m => m.ParametreModule) },
                 ]
             },
-            { path: 'auth', loadChildren: () => import('./features/modules/auth/auth.module').then(m => m.AuthModule) },
+            { path: 'auth', loadChildren: () => import('./features/pages/auth/auth.module').then(m => m.AuthModule) },
             { path: 'notfound', component: NotfoundComponent },
             { path: '**', redirectTo: '/notfound' },
         ])
--- src/app/features/pages/auth/auth-routing.module.ts
+++ src/app/features/pages/auth/auth-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [RouterModule.forChild([
-        { path: 'error', loadChildren: () => import('./components/error/error.module').then(m => m.ErrorModule) },
+        { path: 'error', loadChildren: () => import('../../components/error/error.module').then(m => m.ErrorModule) },
         { path: 'access', loadChildren: () => import('./components/access/access.module').then(m => m.AccessModule) },
         { path: 'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule) },
         { path: '**', redirectTo: '/notfound' }
