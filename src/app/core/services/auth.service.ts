1f8aff7c40c007e6c73e570b33c4812001df6a8e
Donchi Donald <donchidonald2000@gmail.com>
2023-09-04 19:07:50 +0200

    add jwt dependancies

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+    private authPath = environment.apiPath + "/auth";
+    private jwtHelper = new JwtHelperService();
 
   constructor() { }
 }
