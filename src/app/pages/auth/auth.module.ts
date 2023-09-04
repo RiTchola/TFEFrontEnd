039aefc9801b227eb4c2a93bd48bd44e1eb87bcc
Donchi Donald <donchidonald2000@gmail.com>
2023-09-04 18:48:13 +0200

    style update

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import {PasswordModule} from "primeng/password";
+import {FormsModule} from "@angular/forms";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
+import {InputTextModule} from "primeng/inputtext";
+import {DividerModule} from "primeng/divider";
 
 @NgModule({
     imports: [
         CommonModule,
-        AuthRoutingModule
+        AuthRoutingModule,
+        PasswordModule,
+        FormsModule,
+        ButtonModule,
+        RippleModule,
+        InputTextModule,
+        DividerModule
+    ],
+    declarations: [
+      LoginComponent,
+      ChangePasswordComponent
     ]
 })
 export class AuthModule { }
