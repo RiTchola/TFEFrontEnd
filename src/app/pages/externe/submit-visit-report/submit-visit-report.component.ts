1b0ba4c00445b1486e96d0c7232c937d58fbb6e1
RiTchola <yseukam@yahoo.com>
2023-10-01 15:23:09 +0200

    ajustement css

--- src/app/pages/externe/submit-visit-report/submit-visit-report.component.ts
+++ src/app/pages/externe/submit-visit-report/submit-visit-report.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { MenuItem, MessageService } from 'primeng/api';
-import {RapportVisite} from "../../../models/rapport-visite";
+import {RapportVisite, TypePersonne} from "../../../models/rapport-visite";
 import {RapportDeVisiteService} from "../../outils/services/rapport-de-visite.service";
 import {take} from "rxjs";
+import {KeyValue} from "@angular/common";
 
 @Component({
     selector: 'app-submit-visit-report',
@@ -12,7 +13,21 @@ import {take} from "rxjs";
 })
 export class SubmitVisitReportComponent {
     enableForm: boolean = true;
-    typePersonnes: string[] = ["EPOUX","EPOUSE","PARENT","ENFANT","PETIT_FILS","PETITE_FILLE","AUTRE_FAMILLE","AMI","AVOCAT","MEDECIN_TRAITANT","KINESITHERAPEUTE","AUTRE"];
+    typePersonnes: KeyValue<string, TypePersonne>[] = [
+        { key: "Époux", value: "EPOUX" },
+        { key: "Épouse", value: "EPOUSE" },
+        { key: "Parent", value: "PARENT" },
+        { key: "Enfant", value: "ENFANT" },
+        { key: "Petit-Fils", value: "PETIT_FILS" },
+        { key: "Petite-Fille", value: "PETITE_FILLE" },
+        { key: "Membre de la Famille(élargie)", value: "AUTRE_FAMILLE" },
+        { key: "Ami(e)", value: "AMI" },
+        { key: "Avocat(e)", value: "AVOCAT" },
+        { key: "Medecintraitant", value: "MEDECIN_TRAITANT" },
+        { key: "Kinesithérapeute", value: "KINESITHERAPEUTE" },
+        { key: "Autre(s)", value: "AUTRE" }
+    ];
+
     rapportVisite: RapportVisite = {
         id: 0,
         nomResid: '',
@@ -28,6 +43,7 @@ export class SubmitVisitReportComponent {
     }
 
     submit(){
+        console.log(this.rapportVisite)
         this.rapportVisiteService.createRapport(this.rapportVisite).subscribe({
             next: (res)=>{
                 this.enableForm = false;
