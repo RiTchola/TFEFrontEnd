6a70626fa5637595fdb69870c844f92f976e7bda
RiTchola <yseukam@yahoo.com>
2023-10-12 12:19:35 +0200

    qrcode la

--- src/app/pages/outils/outils-routing.module.ts
+++ src/app/pages/outils/outils-routing.module.ts
@@ -4,6 +4,7 @@ import { ImagesVideoComponent } from './images-video/images-video.component';
 import { EtablissementComponent } from './etablissement/etablissement.component';
 import { RapportVisiteComponent } from './rapport-visite/rapport-visite.component';
 import { establissementGuard } from 'src/app/core/guards/auth.guard';
+import { QrcodeComponent } from './qrcode/qrcode.component';
 const routes: Routes = [
     { path: '', redirectTo: 'images-video', pathMatch: 'full' },
     { path: 'images-video', component: ImagesVideoComponent },
